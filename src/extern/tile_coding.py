991db3f63e9a0b404ad8f48c6f66718322fbcdc7
Alex <a.giavaras@gmail.com>
2022-02-15 10:58:08 +0000

    29 Fix test bug

--- src/extern/tile_coding.py
+++ src/extern/tile_coding.py
@@ -92,7 +92,7 @@ def tiles(ihtORsize, numtilings, floats, ints=[], readonly=False):
     return Tiles
 
 
-def tileswrap(ihtORsize, numtilings, floats, wrawidths, ints=[], readonly=False):
+def tileswrap(ihtORsize, numtilings, floats, wrapwidths, ints=[], readonly=False):
     """returns num-tilings tile indices corresponding to the floats and ints, wrapping some floats"""
     qfloats = [floor(f * numtilings) for f in floats]
     Tiles = []
