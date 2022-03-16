ae4cc5796924893012627c7e43b78664f8bd92f9
Alex <a.giavaras@gmail.com>
2022-03-16 16:30:29 +0000

    #75 Update .gitignore

---
+++ src/maths/optimizer_type.py
@@ -0,0 +1,14 @@
+"""Module optimizer_type. Specifies an 
+enumeration for various PyTorch optimizers
+
+"""
+import enum
+
+
+class OptimizerType(enum.IntEnum):
+
+    INVALID = -1
+    GD = 0
+    SGD = 1
+    BatchGD = 2
+    ADAM = 3
